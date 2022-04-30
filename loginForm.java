e48059502ad564bc349dd653acc7ee94e2e66036
mushy005 <78952264+mushy005@users.noreply.github.com>
2022-04-30 17:59:01 +0800

    Update loginForm.java
    ADDED CONNECTION TO ACCOUNT PAGE

--- loginForm.java
+++ loginForm.java
@@ -53,7 +53,7 @@ public class loginForm extends javax.swing.JFrame {
         setSize(new java.awt.Dimension(0, 0));
 
         jPanel1.setBackground(new java.awt.Color(0, 153, 204));
-        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 0, true));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 3, true));
         jPanel1.setMaximumSize(new java.awt.Dimension(500, 350));
         jPanel1.setMinimumSize(new java.awt.Dimension(500, 350));
         jPanel1.setPreferredSize(new java.awt.Dimension(500, 350));
@@ -68,6 +68,7 @@ public class loginForm extends javax.swing.JFrame {
         jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 270, 180, 30));
 
         ButtonLogin.setBackground(new java.awt.Color(255, 204, 204));
+        ButtonLogin.setFont(new java.awt.Font("Swansea", 1, 11)); // NOI18N
         ButtonLogin.setText("Login");
         ButtonLogin.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 153, 255), 1, true));
         ButtonLogin.setBorderPainted(false);
@@ -80,7 +81,7 @@ public class loginForm extends javax.swing.JFrame {
         jPanel1.add(ButtonLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 320, 60, 23));
 
         NewUser.setBackground(new java.awt.Color(255, 255, 255));
-        NewUser.setFont(new java.awt.Font("AvertaStd-Light", 1, 12)); // NOI18N
+        NewUser.setFont(new java.awt.Font("Swansea", 1, 14)); // NOI18N
         NewUser.setForeground(new java.awt.Color(255, 255, 255));
         NewUser.setText("Sign Up Here.");
         NewUser.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -89,19 +90,19 @@ public class loginForm extends javax.swing.JFrame {
                 NewUserMouseClicked(evt);
             }
         });
-        jPanel1.add(NewUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 390, 100, -1));
+        jPanel1.add(NewUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 390, 120, 30));
 
-        jLabel3.setFont(new java.awt.Font("AvertaStd-Regular", 1, 18)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Swansea", 1, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Welcome");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 170, 100, -1));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 170, 100, 30));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ordonez\\Desktop\\logo90.png")); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ordonez\\Desktop\\Java_Contact_App\\Images\\logo90.png")); // NOI18N
         jLabel4.setPreferredSize(new java.awt.Dimension(40, 40));
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 60, 100, 90));
 
         loginClose.setFont(new java.awt.Font("Orbitron", 1, 24)); // NOI18N
-        loginClose.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ordonez\\Desktop\\icon40.png")); // NOI18N
+        loginClose.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ordonez\\Desktop\\Java_Contact_App\\Images\\icon40.png")); // NOI18N
         loginClose.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         loginClose.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -166,13 +167,26 @@ public class loginForm extends javax.swing.JFrame {
         ResultSet rs;
         
         try{
-        ps = con.prepareStatement("SELECT * FROM `user` WHERE `username` = ? AND `pass` = ?");
+        ps = con.prepareStatement("SELECT `username`, `pass`, `pic` FROM `user` WHERE `username` = ? AND `pass` = ?");
         ps.setString(1, TextFieldUsername.getText());
         ps.setString(2, String.valueOf(jPasswordField1.getPassword()));
         rs = ps.executeQuery();
         
         if(rs.next()){
-        JOptionPane.showMessageDialog(null,"Logged");
+        
+            MyContactsForm mcf = new MyContactsForm();
+            mcf.setVisible(true);
+            mcf.pack();
+            mcf.setLocationRelativeTo(null);
+            mcf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+            //Profile pic user
+            mcf.jLabelUserPic.setIcon(new Function().resizePic(null,rs.getBytes (3), mcf.jLabelUserPic.getWidth(), mcf.jLabelUserPic.getHeight()));
+            
+            mcf.jLabelUsername.setText(rs.getString(1));
+            this.dispose();
+            
+            
         }else{
         JOptionPane.showMessageDialog(null,"Login Error");
         
